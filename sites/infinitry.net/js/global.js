0954c0a8b5c3827e582b728c665c34791c58ed9e
Tommy Graves <tommy@infinitry.net>
2013-05-15 16:59:34 -0400

    Infinitry Site 0.2.0 - New billy opening

--- sites/infinitry.net/js/global.js
+++ sites/infinitry.net/js/global.js
@@ -3,7 +3,25 @@
 
     $(function () {
         $(window).resize(function () {}).trigger('resize');
-    
+
+        //Starter
+        $('.nav-starter').click(function (e) {
+            e.preventDefault();
+            var windowHeight = $(window).height(),
+                contentTop = $('#content').position().top
+            $('#content').css('top', contentTop - 171);
+            $('html').removeClass('noscroll');
+            $('header').animate({
+                 bottom: windowHeight-70,
+                 'background-color': 'rgba(25, 51, 95, .85)'
+            }, 750).find('.nav-starter').fadeOut(500);
+            $('nav').animate({
+                right: 0
+            }, 750);
+            $('#billboard-text').animate({top: -342}, 750);
+            $('#content').animate({top: -171}, 750);
+            // $('html, body').stop().animate({scrollTop: windowHeight-70}, 750);
+        });
         
         //IE < 10 doesn't support the placeholder attribute, so hack it in.
         $('.ie input, .ie textarea').each(function () {
