4b1995676dea404e84dd09c19f75d167192dc8f3
Tommy Graves <tommy@infinitry.net>
2013-05-15 23:39:23 -0400

    Infinitry Site 0.2.3 - Bugfixes & Parallax

--- sites/infinitry.net/js/global.js
+++ sites/infinitry.net/js/global.js
@@ -15,7 +15,7 @@
              bottom: windowHeight-70,
              'background-color': 'rgba(25, 51, 95, .85)'
         }, 750, function () {
-            $(this).css({
+            $(this).css({ //We change to a non-calculated property here for screen resizing.
                 bottom: 'auto',
                 top: 0
             });
@@ -27,7 +27,7 @@
         
         $('#billboard-text').animate({top: -342}, 750); //slide billboard text out
         
-        content.animate({top: -171}, 750, function () {
+        content.animate({top: -171}, 750, function () { //slide content in
             if (fnSet) {
                 fn(); //run callback once content is visible
             } else {
@@ -49,9 +49,9 @@
             right: -2000
         }, 750);
         
-        $('header').css({
+        $('header').css({ //back to a calculated property to avoid any difficulties
             top: 'auto',
-            bottom: windowHeight-70
+            bottom: windowHeight-70 
         }).animate({ //move nav to bottom, change its bg, and show the button
              bottom: 0,
              'background-color': 'rgba(0, 0, 0, .9)'
@@ -60,68 +60,55 @@
         $('html').addClass('noscroll'); //disable scrolling
     };
     
-    var route = function (state) {
-        var scrollTo = $('#' + state).position().top;
-        $('html, body').stop().animate({scrollTop: scrollTo-70}, 500);
+    var route = function (state) { //scrolls to the state's section
+        var scrollTo = $('#' + state).position().top; //get location of the section
+        $('html, body').stop().animate({scrollTop: scrollTo-70}, 500); //scroll there, leave room for the menu
     }
-    
-    var History = window.History;
-    
-    History.silentStateChange = function (data, title, state) {
-        data.silent = true;
-        History.replaceState(data, title, state);
-    }
-    
-    History.Adapter.bind(window,'statechange',function(){
-        var State = History.getState();
-        if (!State.data.silent) {
-            if (State.data.state === "splash" || typeof State.data.state === "undefined") {
-                restart();
-            } else {
-                if ($('.nav-starter').is(':visible')) { //splash screen is showing
-                    start(function () {
-                        route(State.data.state);
-                    });
-                } else {
-                    route(State.data.state);
-                }
+        
+    $(window).bind('statechange', function () { //when state is changed
+        var State = History.getState(); 
+        if (State.data.state === "splash" || typeof State.data.state === "undefined") { //if we're moving to the splash page
+            restart(); //in case we hit the back button. no consequences for first load
+        } else { //non-splash page
+            if ($('.nav-starter').is(':visible')) { //splash screen is showing (e.g. we refreshed)
+                start(function () { //get rid of the splash page
+                    route(State.data.state); //move to the right section
+                });
+            } else { //menu link or forward/back
+                route(State.data.state); //move to the right section
             }
         }
     });
     
     $(function () {
-        $('html, body').scrollTop(0);
-        //if index.html, push splash state; otherwise, route to correct state
+
+        //route to the current section
         var state = History.getState().data.state;
-        
-        if (typeof state !== "undefined" && state !== "splash") {
-            //History.silentStateChange({state: 'splash'}, "Infinitry", "?state=splash");
-        //} else {
+        if (typeof state !== "undefined" && state !== "splash") { //not splash page
             start(function () {
-                route(state);   
+                route(state);
             });
         };
         
+        //menu link functionality
         $('nav a').click(function (e) {
-            e.preventDefault();
-            var state = $(this).attr('href').split('?state=')[1],
-                title = $('#' + state).data('statetitle');
+            e.preventDefault(); //stop from loading hashtag page
+            var state = $(this).attr('href').split('?state=')[1], //state in href
+                title = $('#' + state).data('statetitle'); //menu sections have this data attr
             if (state === History.getState().data.state) {
-                route(state);
+                route(state); //scroll to top of already selected section
             } else {
                 History.pushState({state: state}, title, '?state=' + state);
             }
         });
-        
-        $(window).resize(function () {
-        }).trigger('resize');
 
-        $(window).scroll(function () { //scrolling magic for URLs
-            var sectionInView = $('.container:in-viewport').parent('[data-statetitle]'),
+        //change menu as we scroll
+        $(window).scroll(function () {
+            var sectionInView = $('.container:in-viewport').parent('[data-statetitle]'), //only menu items
                 title = sectionInView.data('statetitle'),
                 state = sectionInView.attr('id');
             
-            if (typeof title !== "undefined") {
+            if (typeof title !== "undefined") { //just in case something else got selected somehow; probably unnecessary
                 $('nav a.active').removeClass('active');
                 $('nav a[href="?state=' + state + '"]').addClass('active');
             }
